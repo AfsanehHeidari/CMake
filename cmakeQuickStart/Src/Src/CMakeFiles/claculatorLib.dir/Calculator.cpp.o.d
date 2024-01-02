11362a55bf037e98331c8da33207c2f035d2fdd4
Afsaneh.Heidari <143424548+AfsanehHeidari@users.noreply.github.com>
2024-01-02 14:00:21 +0200

    Delete cmakeQuickStart/Src/Src directory

--- cmakeQuickStart/Src/Src/CMakeFiles/claculatorLib.dir/Calculator.cpp.o.d
+++
@@ -1,4 +0,0 @@
-Src/CMakeFiles/claculatorLib.dir/Calculator.cpp.o: \
- /tmp/project1/CMake/cmakeQuickStart/Src/Calculator.cpp \
- /usr/include/stdc-predef.h \
- /tmp/project1/CMake/cmakeQuickStart/Src/include/Calculator.h
